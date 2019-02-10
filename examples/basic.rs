3aca5e8aaa597d67b018a7c8169a769091feab49
Suyash <suyash93@protonmail.com>
2019-02-10 17:43:26 +0530

    examples: update to 2018

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,5 +1,3 @@
-extern crate geopattern;
-
 fn main() {
     let p = geopattern::generate("something wicked this way comes");
     println!("{}", p.to_svg().unwrap());
--- examples/samples.rs
+++ examples/samples.rs
@@ -1,6 +1,3 @@
-extern crate geopattern;
-extern crate svg;
-
 use geopattern::patterns::Patterns;
 
 fn main() {
